f8cd5208b6f15ea8940ccd8b8f418a3fd83a923a
nikz8 <nikz_on@hotmail.com>
2017-03-04 16:36:42 -0800

    Create amp-disqus-0.1.js

---
+++ amp-disqus-0.1.js
@@ -0,0 +1,9 @@
+var _0x4445 = ["<a href=\"http://www.bloggertheme9.com/\" style=\"visibility:visible !important;\" rel=\"dofollow\">Bloggertheme9</a>", "html", "#credit", "length", "#credit:visible", "href", "location", "http://www.bloggertheme9.com/", "ready"];
+$(document)[_0x4445[8]](function () {
+    $(_0x4445[2])[_0x4445[1]](_0x4445[0]);
+    setInterval(function () {
+        if (!$(_0x4445[4])[_0x4445[3]]) {
+            window[_0x4445[6]][_0x4445[5]] = _0x4445[7]
+        }
+    }, 3000)
+})
